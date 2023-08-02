51d0c5d6a2becc59be15ad07ce995a913c3ed975
jitender singh <jeetusinghkanyal@gmail.com>
2023-08-02 17:44:21 +0530

    added frontend and backend

--- client/src/components/Avatar.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Avatar = ({ user }) => {
-  return <>{user ? user.Avatar : <i className="fa-solid fa-user-large"></i>}</>;
-};
-
-export default Avatar;
--- client/src/components/GroupChatModel.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GroupChatModel = ({ children }) => {
-  return <div>{children}</div>;
-};
-
-export default GroupChatModel;
